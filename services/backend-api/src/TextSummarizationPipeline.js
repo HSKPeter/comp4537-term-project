35842cc12564092130f8fcdbafb8555b3e58308e
Peter Hui <huishikuen@gmail.com>
2023-11-15 23:15:08 -0800

    Setup backend to use local ML model

---
+++ services/backend-api/src/TextSummarizationPipeline.js
@@ -0,0 +1,20 @@
+import { pipeline, env } from '@xenova/transformers';
+
+class TextSummarizationPipeline {
+    static task = 'summarization'
+    static model = 'Xenova/distilbart-cnn-6-6'
+    static instance = null;
+
+    static async getInstance(progress_callback = null) {
+        if (this.instance === null) {
+            env.localModelPath = './distilbart-cnn-6-6';
+            env.allowRemoteModels = false;
+
+            this.instance = pipeline(this.task, this.model, { progress_callback });
+        }
+
+        return this.instance;
+    }
+}
+
+export { TextSummarizationPipeline };
