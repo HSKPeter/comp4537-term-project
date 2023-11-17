27f2d749ae1794299f864b5689e52a2c1170a9e8
Peter Hui <huishikuen@gmail.com>
2023-11-17 13:19:52 -0800

    Reorganize config constants

--- services/backend-api/src/config.js
+++ services/backend-api/src/config.js
@@ -1,27 +1,35 @@
 require('dotenv').config();  // Read .env file if it exists
 
-const MODES = {
-    development: 'development',
-    production: 'production'
-}
+// TODO: Update production configs
+const PRODUCTION_CONFIGS = {
+    backendOrigin: 'TBC',
+    frontendOrigin: 'TBC',
+    port: 80,
+};
 
-const IS_DEVELOPMENT_MODE = process.env.NODE_ENV === MODES.development;
-
-const MODE = IS_DEVELOPMENT_MODE ? MODES.development : MODES.production;
+const LOCAL_HOST = 'http://localhost';
 
 const PORTS = {
     development: {
         frontend: 3000,
         backend: 8080
     },
-    production: 80
+    production: PORT_IN_PRODUCTION
 };
+
+const MODES = {
+    development: 'development',
+    production: 'production'
+};
+
+const IS_DEVELOPMENT_MODE = process.env.NODE_ENV === MODES.development;
+
+const MODE = IS_DEVELOPMENT_MODE ? MODES.development : MODES.production;
 
 const PORT = IS_DEVELOPMENT_MODE ? PORTS.development.backend : PORTS.production;
 
-const LOCAL_HOST = 'http://localhost';
-const FRONTEND_ORIGIN = IS_DEVELOPMENT_MODE ? `${LOCAL_HOST}:${PORTS.development.frontend}` : 'TODO';
-const BACKEND_ORIGIN = IS_DEVELOPMENT_MODE ? `${LOCAL_HOST}:${PORTS.development.backend}` : 'TODO';
+const FRONTEND_ORIGIN = IS_DEVELOPMENT_MODE ? `${LOCAL_HOST}:${PORTS.development.frontend}` : PRODUCTION_CONFIGS.frontendOrigin;
+const BACKEND_ORIGIN = IS_DEVELOPMENT_MODE ? `${LOCAL_HOST}:${PORTS.development.backend}` : PRODUCTION_CONFIGS.backendOrigin;
 
 const WHITE_LIST_ORIGINS = [FRONTEND_ORIGIN, BACKEND_ORIGIN];
 
