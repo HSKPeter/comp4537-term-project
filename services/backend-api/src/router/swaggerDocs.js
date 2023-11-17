678e071c8b8a4b80d6fbe1c87df9a9934346c304
Peter Hui <huishikuen@gmail.com>
2023-11-17 12:12:44 -0800

    Specify server url in swagger doc

--- services/backend-api/src/router/swaggerDocs.js
+++ services/backend-api/src/router/swaggerDocs.js
@@ -1,9 +1,11 @@
 const swaggerJsdoc = require("swagger-jsdoc");
 const YAML = require('yamljs');
 const path = require('path');
+const { BACKEND_ORIGIN } = require('../config');
 
 const swaggerYamlFilePath = path.join(__dirname, 'swagger.yaml');
 const swaggerDefinition = YAML.load(swaggerYamlFilePath);
+swaggerDefinition.servers = [{ url: BACKEND_ORIGIN }];
 
 const options = {
     swaggerDefinition,
