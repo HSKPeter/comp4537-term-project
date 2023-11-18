f37206698cd269c728732fbb836b3a3f2a2155a9
james01123 <97496209+james01123@users.noreply.github.com>
2023-11-18 14:27:35 -0800

    Implemented register and login functionality.
    JWT_SECRET_KEY is stored in .env (not visible)
    
    TODO: implement admin and regular user specification
    
    - implemet token verification for backend
    - add api calls as info to JWT body

--- services/authentication/src/server.js
+++ services/authentication/src/server.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const cors = require('cors');
 const mysql = require('mysql2');
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcrypt');
+require('dotenv').config({ path: './.env' });
+
 
 const dbPool = mysql.createPool({
-    host: "localhost", 
+    host: "localhost",
     user: "root",
     password: "",
-    database: "comp4537_project"
+    database: "comp4537_project",
+    port: 4306
 });
 
 const CREATE_TABLE_QUERIES = {
     User: `
         CREATE TABLE IF NOT EXISTS User (
-            UserID INT PRIMARY KEY,
+            UserID INT PRIMARY KEY AUTO_INCREMENT,
             Name VARCHAR(100),
             Password VARCHAR(100),
             UserType INT,
@@ -21,13 +26,13 @@ const CREATE_TABLE_QUERIES = {
     `,
     UserType: `
         CREATE TABLE IF NOT EXISTS UserType (
-            UserTypeID INT PRIMARY KEY,
+            UserTypeID INT PRIMARY KEY AUTO_INCREMENT,
             UserAuthorization VARCHAR(100)
         );
     `,
     APICall: `
         CREATE TABLE IF NOT EXISTS APICall (
-            API_Call_ID INT PRIMARY KEY,
+            API_Call_ID INT PRIMARY KEY AUTO_INCREMENT,
             UserID INT,
             Time DATETIME,
             FOREIGN KEY (UserID) REFERENCES User(UserID)
@@ -38,16 +43,61 @@ const CREATE_TABLE_QUERIES = {
 const app = express();
 
 app.use(cors());
+app.use(express.json());
 
 const PORT = 8000;
+const SECRET_KEY = process.env.JWT_SECRET_KEY;
 
+app.post('/register', async (req, res) => {
+    const { username, password } = req.body;
 
-app.post('/register', (req, res) => {
-    
+    try {
+        // Check if the username already exists
+        const userExists = await runSQLQuery(`SELECT * FROM User WHERE Name = ?`, [username]);
+
+        if (userExists.length > 0) {
+            return res.status(400).json({ error: 'Username already exists' });
+        }
+
+        // Hash the password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Insert the user into the database
+        await runSQLQuery('INSERT INTO User (Name, Password) VALUES (?, ?)', [username, hashedPassword]);
+
+        res.status(201).json({ message: 'User registered successfully' });
+    } catch (error) {
+        console.error('Error registering user: ', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
-app.post('/login', (req, res) => {
-    
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        // Check if the username exists
+        const user = await runSQLQuery('SELECT * FROM User WHERE Name = ?', [username]);
+
+        if (user.length === 0) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        // Compare the provided password with the hashed password in the database
+        const passwordMatch = await bcrypt.compare(password, user[0].Password);
+
+        if (!passwordMatch) {
+            return res.status(401).json({ error: 'Invalid username or password' });
+        }
+
+        // Sign a JWT token
+        const token = jwt.sign({ userID: user[0].UserID, username: user[0].Name }, SECRET_KEY, { expiresIn: '1h' });
+
+        res.status(200).json({ token });
+    } catch (error) {
+        console.error('Error logging in: ', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
 });
 
 app.listen(PORT, () => {
@@ -59,15 +109,15 @@ app.listen(PORT, () => {
     }
 });
 
-function runSQLQuery(sqlQuery) {
+function runSQLQuery(sqlQuery, params = []) {
     return new Promise((resolve, reject) => {
-        dbPool.query(sqlQuery, (err, results) => {
+        dbPool.query(sqlQuery, params, (err, results) => {
             if (err) {
                 return reject(err);
             }
             return resolve(results);
         });
-    }) 
+    })
 }
 
 async function setupDatabase() {
