7980b215e1b413a210a5c67b1d80d33ed9879d8e
Peter Hui <huishikuen@gmail.com>
2023-11-23 15:31:53 -0800

    Create news content controller

---
+++ services/backend-api/src/controllers/newsContent.js
@@ -0,0 +1,26 @@
+const { USER_MESSAGES } = require('../messages/userMessage');
+const { vsprintf } = require('sprintf-js');
+const { SERVER_MESSAGES } = require('../messages/serverMessage');
+const { HTTP_STATUS_CODES } = require('../utils/httpUtils');
+const { getNews } = require('../utils/newsUtils');
+
+async function newsContentController(req, res) {
+    const { q: keyword } = req.query;
+    if (keyword === undefined) {
+        res.status(HTTP_STATUS_CODES.BAD_REQUEST).json({ error: USER_MESSAGES.missingQuery });
+        return;
+    }
+
+    getNews(keyword)
+        .then((news) => {
+            res.json(news);
+        })
+        .catch((err) => {
+            console.error(vsprintf(SERVER_MESSAGES.failedToGetNews, [err?.stack ?? err]))
+            res.status(HTTP_STATUS_CODES.INTERNAL_SERVER_ERROR).json({ error: USER_MESSAGES.failedToGetNews });
+        });
+}
+
+module.exports = {
+    newsContentController
+}
--- services/backend-api/src/messages/userMessage.js
+++ services/backend-api/src/messages/userMessage.js
@@ -2,7 +2,9 @@ const USER_MESSAGES = {
     test: 'Hello World!', // TODO: Remove this line
     generalError: 'Something went wrong. Please try again later.',
     missingText: 'Missing text. Please provide text to summarize.',
+    missingQuery: 'Missing query. Please provide a query to search for.',
     failedToSummarizeText: 'Failed to summarize text. Please try again later.',
+    failedToGetNews: 'Failed to get news. Please try again later.',
 };
 
 module.exports = {
