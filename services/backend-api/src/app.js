0b25e7e7d03e683e7aa85b56da0e8c0bf086dae3
Peter Hui <huishikuen@gmail.com>
2023-11-24 11:50:59 -0800

    Refactor backend code

--- services/backend-api/src/app.js
+++ services/backend-api/src/app.js
@@ -1,24 +1,27 @@
 const express = require('express');
 const cors = require('cors');
-const bodyParser = require('body-parser');
 const swaggerUi = require('swagger-ui-express');
 
 const { ROUTE_PATHS } = require('./router/routes');
 const { swaggerSpecs } = require('./router/swaggerDocs');
 const { CORS_OPTIONS } = require('./utils/corsUtils');
 const router = require('./router');
+const { IS_DEVELOPMENT_MODE } = require('./config');
 
 const app = express();
 
 app.use(ROUTE_PATHS.SWAGGER, swaggerUi.serve, swaggerUi.setup(swaggerSpecs));
 
 // Enable CORS
-// app.use(cors(CORS_OPTIONS)); 
-app.use(cors()); // TODO: Remove this line and uncomment the above line
+if (IS_DEVELOPMENT_MODE) {
+    app.use(cors());
+} else {
+    app.use(cors(CORS_OPTIONS));
+}
 
-// Enable body parser
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// Enable body parsing for JSON and URL encoded data
+app.use(express.json()) // for parsing application/json
+app.use(express.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
 
 app.use(ROUTE_PATHS.ROOT, router);
 
