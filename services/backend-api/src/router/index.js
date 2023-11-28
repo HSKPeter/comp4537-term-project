fed2e08736454a4a6f514253583c88928a38ceca
Peter Hui <huishikuen@gmail.com>
2023-11-27 16:09:04 -0800

    Disable checkUserQuota in dev mode

--- services/backend-api/src/router/index.js
+++ services/backend-api/src/router/index.js
@@ -7,15 +7,18 @@ const {
     userRegistrationController
 } = require('../controllers');
 const { checkUserQuota } = require('../middlewares/checkUserQuota');
+const { IS_PRODUCTION_MODE } = require('../config');
 
 const router = Router();
 
 router.post(API_ROUTE_PATHS.LOGIN, userLoginController);
 router.post(API_ROUTE_PATHS.REGISTER, userRegistrationController);
 
-router.use(checkUserQuota);
+if (IS_PRODUCTION_MODE) {  // TODO: Remove this check once the auth server is implemented
+    router.use(checkUserQuota);
+}
 
-// The routes below require a valid token to be present either in the Authorization header or in the cookies
+// In production, the routes below require a valid token to be present either in the Authorization header or in the cookies
 router.get(API_ROUTE_PATHS.NEWS_CONTENT, newsContentController);
 router.post(API_ROUTE_PATHS.SUMMARIZE_TEXT, textSummarizationController);
 
