51c5857ad9e80cfb84fcb21288b4aee96014a75f
Sepehr Zohoori Rad <zohooris1@gmail.com>
2023-11-28 14:56:27 -0800

    admin rerouting

--- services/frontend/src/AdminPage.js
+++ services/frontend/src/AdminPage.js
@@ -1,8 +1,33 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { getUserRole, getUserRoleFromCache } from './utils/userRoleUtils';
 
 
 
 function AdminPage() {
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    useEffect(() => {
+        async function navigateToLoginPageIfRoleNotFound() {
+            let role = getUserRoleFromCache();
+
+            if (!role) {
+                navigate('/login', { state: { from: location } });
+                return;
+            }
+
+            role = await getUserRole();
+            if (!role) {
+                navigate('/login', { state: { from: location } });
+                return;
+            }
+        }
+
+        navigateToLoginPageIfRoleNotFound();
+    }, []);
+    
     return (
         <div className='admin-page'>
             <h1>Admin Page</h1>
