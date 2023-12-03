55556f2de1011a8c8735c6a4aaf39312f25664e0
Peter Hui <huishikuen@gmail.com>
2023-12-02 19:41:02 -0800

    Fix cookie clearing issue

--- services/backend-api/src/controllers/userLogout.js
+++ services/backend-api/src/controllers/userLogout.js
@@ -1,8 +1,8 @@
 const { HTTP_STATUS_CODES } = require('../utils/httpUtils');
-const { COOKIE_KEYS } = require('../utils/cookieUtils');
+const { COOKIE_KEYS, COOKIE_CONFIG } = require('../utils/cookieUtils');
 
 function userLogoutController(_req, res) {
-  res.clearCookie(COOKIE_KEYS.TOKEN);
+  res.clearCookie(COOKIE_KEYS.TOKEN, COOKIE_CONFIG);
   res.status(HTTP_STATUS_CODES.NO_CONTENT);
   res.end();
 }
