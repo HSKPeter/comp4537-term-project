d3546125183ffca097e4d31d59a33672e78f75e4
james01123 <97496209+james01123@users.noreply.github.com>
2023-12-03 19:08:19 -0800

    implemented needed methods in apiStats.js and tokenUtils.js

--- services/backend-api/src/controllers/apiStats.js
+++ services/backend-api/src/controllers/apiStats.js
@@ -1,18 +1,17 @@
 const { vsprintf } = require("sprintf-js");
 const { SERVER_MESSAGES } = require("../messages/serverMessage"); // Assuming you have a server messages module
 const { HTTP_STATUS_CODES } = require("../utils/httpUtils");
-const { getRoleFromToken } = require("../utils/tokenUtils"); // Assuming you have a function to get role from token
-const { getUserToken } = require("../utils/tokenUtils"); // Assuming you have a function to get user token
-const { getApiStats, getApiStatsFromUser, getApiConsumptionForCurrentUser } = require("../utils/adminUtils");
+const { readRole } = require("../utils/tokenUtils"); // Assuming you have a function to get user token
+const { getApiStats, getApiStatsByUser, getApiConsumptionStats } = require("../utils/adminUtils");
 
 async function apiStatsController(req, res) {
     try {
-        // Assuming you have a function to retrieve the JWT token from the request
-        const userToken = getUserToken(req);
 
-        // Validate the user's role (assuming 'admin' is required)
-        const userRole = await getRoleFromToken(userToken);
-        if (userRole !== 'admin') { // Assuming 'admin' is the required role
+        const token = req.cookies.token;
+        const role = await readRole(token);
+        console.log(role);
+
+        if (role !== 'admin') { // Assuming 'admin' is the required role
             res.status(HTTP_STATUS_CODES.FORBIDDEN).json({ error: SERVER_MESSAGES.accessDenied });
             return;
         }
@@ -31,18 +30,18 @@ async function apiStatsController(req, res) {
 
 async function apiStatsByUserController(req, res) {
     try {
-        // Assuming you have a function to retrieve the JWT token from the request
-        const userToken = getUserToken(req);
 
-        // Validate the user's role (assuming 'admin' is required)
-        const userRole = await getRoleFromToken(userToken);
-        if (userRole !== 'admin') { // Assuming 'admin' is the required role
+        const token = req.cookies.token;
+        const role = await readRole(token);
+        console.log(role);
+
+        if (role !== 'admin') { // Assuming 'admin' is the required role
             res.status(HTTP_STATUS_CODES.FORBIDDEN).json({ error: SERVER_MESSAGES.accessDenied });
             return;
         }
 
         // Fetch API stats by user
-        const apiStatsByUser = await getApiStatsFromUser();
+        const apiStatsByUser = await getApiStatsByUser();
 
         // Return API stats by user
         res.status(HTTP_STATUS_CODES.OK).json({ usageStats: apiStatsByUser });
@@ -55,14 +54,11 @@ async function apiStatsByUserController(req, res) {
 
 async function apiConsumptionController(req, res) {
     try {
-        // Assuming you have a function to retrieve the token from the request
-        const token = getUserToken(req);
-
-        // Extract userID from the request parameters
-        const userId = readUserId(token); // Assuming you have a function to read user ID from token
+        const token = req.cookies.token;
+        const userId = readUserId(token);
 
         // Fetch API consumption stats for the specified user ID
-        const apiConsumptionStats = await getApiConsumptionForCurrentUser(userId);
+        const apiConsumptionStats = await getApiConsumptionStats(userId);
 
         // Return API consumption stats
         res.status(HTTP_STATUS_CODES.OK).json({ usageStats: apiConsumptionStats });
--- services/backend-api/src/utils/tokenUtils.js
+++ services/backend-api/src/utils/tokenUtils.js
@@ -10,6 +10,17 @@ function readUserId(jwtToken) {
     return decodedToken.userID;
 }
 
+function readRole(jwtToken) {
+    const decodedToken = jwt.decode(jwtToken);
+
+    if (!decodedToken) {
+        return null;
+    }
+    
+    return decodedToken.role;
+}
+
 module.exports = {
     readUserId,
+    readRole
 };
