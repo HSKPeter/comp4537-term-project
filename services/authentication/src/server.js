abe711697f0cefe4420b176a8c29e15f7f3f7d2f
pcammayo <pcammayo03@gmail.com>
2023-12-04 22:38:58 -0800

    Created register.js

--- services/authentication/src/server.js
+++ services/authentication/src/server.js
@@ -7,7 +7,9 @@ const swaggerUi = require('swagger-ui-express');
 require('dotenv').config({ path: './.env' });
 
 require('./router/routes');
-const { runSQLQuery, setupDatabase, UserTypes } = require('./utils/sqlUtil')
+const { runSQLQuery, setupDatabase } = require('./utils/sqlUtil')
+const { registerController } = require('./controllers/register');
+const { loginController } = require('./controllers/login');
 const { swaggerSpecs } = require('./swagger/swaggerDocs');
 const { ROUTE_PATHS } = require('./router/routes');
 
@@ -73,48 +75,7 @@ app.use((req, res, next) => {
     next();
 })
 
-app.post(ROUTE_PATHS.REGISTER, async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-
-        // Check if the username already exists
-        const userExists = await runSQLQuery(`SELECT * FROM User WHERE Name = ?`, [username]);
-
-        if (userExists.length > 0) {
-            return res.status(500).json({ error: 'Username already exists' });
-        }
-
-        // Hash the password
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        // Insert the user into the database
-        await runSQLQuery('INSERT INTO User (Name, Password, UserType, Email) VALUES (?, ?, ?, ?)', [username, hashedPassword, UserTypes.Regular, email]);
-
-        // Select the user from the database
-        const user = await runSQLQuery('SELECT * FROM User WHERE Name = ?', [username]);
-
-        // Obtain the user type from the database        
-        let queryResult = await runSQLQuery(`
-            SELECT UserAuthorization 
-            FROM UserType 
-            WHERE UserTypeID = (
-                SELECT UserType AS SelectedUserType 
-                FROM User 
-                WHERE Name = ?
-            )`,
-            [username]
-        );
-        
-        const role = queryResult[0].UserAuthorization;
-
-        const token = jwt.sign({ userID: user[0].UserID, username: user[0].Name, role }, SECRET_KEY, { expiresIn: DEFAULT_TOKEN_EXPIRES_IN });
-        res.status(201).json({ message: 'User registered successfully', token, role });
-    } catch (error) {
-        console.error('Error registering user: ', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
+app.post(ROUTE_PATHS.REGISTER, );
 app.post(ROUTE_PATHS.LOGIN, async (req, res) => {
     try {
         const { email, username, password } = req.body;
