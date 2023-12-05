a9918333f03c5393592bbc8caa4faabd80c7a7a9
pcammayo <pcammayo03@gmail.com>
2023-12-04 23:03:34 -0800

    Implemented bookmarkWordControllers.

--- services/authentication/src/server.js
+++ services/authentication/src/server.js
@@ -13,7 +13,12 @@ const { loginController } = require('./controllers/login');
 const { userController } = require('./controllers/user');
 const { roleController } = require('./controllers/role');
 const { recordController } = require('./controllers/record');
-const { bookmarkWordsController } = require('./controllers/bookmarkWords');
+const { 
+    getBookmarkWordController, 
+    postBookmarkWordController,  
+    putBookmarkWordController,
+    deleteBookmarkWordController
+} = require('./controllers/bookmarkWords');
 const { apiStatsController } = require('./controllers/apiStats');
 const { apiStatsByUserController } = require('./controllers/apiStatsByUser');
 const { apiConsumptionController } = require('./controllers/apiConsumption');
@@ -89,157 +94,11 @@ app.post(ROUTE_PATHS.USER, userController);
 app.post(ROUTE_PATHS.ROLE, roleController);
 app.post(ROUTE_PATHS.RECORD, recordController);
 
-app.get(ROUTE_PATHS.GET_BOOKMARK_WORDS, async (req, res) => {
-    try {
-        const {userID} = req.params;
-        runSQLQuery('SELECT word FROM BookmarkWord WHERE UserID = ?', [userID])
-        .then((wordsQueryResults) => {
-            const words = wordsQueryResults.map((wordQueryResult) => wordQueryResult.word);
-            // Set Cache-Control header to prevent caching
-            res.setHeader('Cache-Control', 'no-store');
-            res.status(200).json({ words });
-        })
-        .catch((err) => {
-            console.error('Error retrieving bookmarked words: ', err);
-            res.status(500).json({ error: 'Internal server error' });
-        });
-    } catch (error) {
-        console.error('Error retrieving bookmarked words: ', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-app.post(ROUTE_PATHS.POST_BOOKMARK_WORDS, async (req, res) => {
-    try {
-        const {userID} = req.params;
-    
-        const { word } = req.body;
-
-        if (word.length === 0) {
-            res.status(409).json({ error: 'Word is required' });
-            return;
-        }
-
-        // Check if word exists in the database
-        let wordExists = await runSQLQuery('SELECT * FROM BookmarkWord WHERE UserID = ? AND Word = ?', [userID, word]);
-        if (wordExists.length > 0) {
-            res.status(409).json({ error: `Word '${word}' already bookmarked` });
-            return;
-        }
-
-        console.log(`Bookmarking word: ${word}`);
-
-        runSQLQuery('INSERT INTO BookmarkWord (UserID, Word) VALUES (?, ?)', [userID, word])
-            .catch((err) => {
-                console.error('Error bookmarking word: ', err);
-                res.status(500).json({ error: 'Internal server error' });
-                return;
-            });
-
-        res.status(201).json({ message: `"${word}" bookmarked successfully` });
+app.get(ROUTE_PATHS.GET_BOOKMARK_WORDS, getBookmarkWordController);
+app.post(ROUTE_PATHS.POST_BOOKMARK_WORDS, postBookmarkWordController); 
+app.put(ROUTE_PATHS.PUT_BOOKMARK_WORDS, putBookmarkWordController);
+app.delete(ROUTE_PATHS.DELETE_BOOKMARK_WORDS, deleteBookmarkWordController); 
         
-    } catch (error) {
-        console.error('Error bookmarking word: ', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-app.put(ROUTE_PATHS.PUT_BOOKMARK_WORDS, async (req, res) => {
-    try {
-        const {userID} = req.params;
-
-        const { originalWord, newWord } = req.body;
-
-        // Check if original word exists
-        let wordExists = await runSQLQuery('SELECT * FROM BookmarkWord WHERE UserID = ? AND Word = ?', [userID, originalWord]);
-        if (wordExists.length === 0) {
-            res.status(400).json({ error: `Word '${originalWord}' not found` });
-            return;
-        }
-    
-        let rows = runSQLQuery('UPDATE BookmarkWord SET Word = ? WHERE UserID = ? AND Word = ?', [newWord, userID, originalWord])
-            .then(() => {
-                res.status(200).json({ message: 'Word updated successfully' });
-            })
-            .catch((err) => {
-                console.error('Error updating bookmarked word: ', err);
-                res.status(500).json({ error: 'Internal server error' });
-            });
-    } catch (error) {
-        console.error('Error updating bookmarked word: ', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-app.delete(ROUTE_PATHS.DELETE_BOOKMARK_WORDS, async (req, res) => {
-    try {
-        const {userID} = req.params;
-
-        const toDeleteAll = req.query.all === 'true';
-    
-        if (toDeleteAll) {
-            runSQLQuery('DELETE FROM BookmarkWord WHERE UserID = ?', [userID])
-            .then(() => {
-                res.status(200).json({ message: 'All words deleted successfully' });
-            })
-            .catch((err) => {
-                console.error('Error deleting all bookmarked words: ', err);
-                res.status(500).json({ error: 'Internal server error' });
-            });
-            return;
-        } 
-    
-        const { word } = req.body;
-
-        // Check if the words array is empty
-        if (!word) {
-            res.status(400).json({ error: 'Received an empty string!' });
-            return;
-        }
-
-        // Checks if the word exists in the database
-        let wordExists = await runSQLQuery('SELECT * FROM BookmarkWord WHERE UserID = ? AND Word = ?', [userID, word]);
-        if (wordExists.length === 0) {
-            res.status(400).json({ error: `Word '${word}' not found` });
-            return;
-        }
-        
-
-        console.log(`Deleting word: ${word}`);
-
-        // Delete the word from the database
-        runSQLQuery('DELETE FROM BookmarkWord WHERE UserID = ? AND Word = ?', [userID, word])
-            .catch((err) => {
-                console.error('Error deleting bookmarked word: ', err);
-                res.status(500).json({ error: 'Internal server error' });
-            });
-
-        // for (let i = 0; i < words.length; i++) {
-        //     const word = words[i];
-        //     let wordExists = await runSQLQuery('SELECT * FROM BookmarkWord WHERE UserID = ? AND Word = ?', [userID, word]);
-        //     if (wordExists.length === 0) {
-        //         res.status(400).json({ error: `Word '${word}' not found` });
-        //         return;
-        //     }
-
-        //     console.log(`Deleting word: ${word}`)
-
-        //     runSQLQuery('DELETE FROM BookmarkWord WHERE UserID = ? AND Word = ?', [userID, word])
-        //         .catch((err) => {
-        //             console.error('Error deleting bookmarked word: ', err);
-        //             res.status(500).json({ error: 'Internal server error' });
-        //         });
-        // }
-
-        res.status(200).json({ message: 'Words deleted successfully' });
-    } catch (error) {
-        console.error('Error deleting bookmarked word: ', error);
-        res.status(500).json({ error: 'Internal server error' });
-    }
-});
-
-
-// Admin API Endpoints
 
 //Implement get API stats for admin
 app.get(ROUTE_PATHS.API_STATS, async (req, res) => {
