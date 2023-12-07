77fb6e32605f7b1307f047ce46bdcd4a5ad75186
Sepehr Zohoori Rad <zohooris1@gmail.com>
2023-12-07 15:53:14 -0800

    fixed some random function export

--- services/frontend/src/utils/warningUtils.js
+++ services/frontend/src/utils/warningUtils.js
@@ -1,12 +1,10 @@
 import { USER_MESSAGES_EN } from './userMessages.js';
 
-function displayWarningIfExceedApiLimit(response) {
+const displayWarningIfExceedApiLimit = (response) => {
     const isApiLimitExceeded = response.headers['x-api-limit-exceeded'] === 'true';
     if (isApiLimitExceeded) {
         alert(USER_MESSAGES_EN.warning_utils_api_limit_exceeded);
     }
 }
 
-module.exports = {
-    displayWarningIfExceedApiLimit
-}
+export { displayWarningIfExceedApiLimit };
